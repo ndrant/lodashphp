076dd029285f705eca101e28f76bfdf341141c6c
indra heryanto <indrasids74@gmail.com>
2023-09-20 17:27:53 +0800

    first commit

---
+++ src/Lodashphp.php
@@ -0,0 +1,15 @@
+<?php
+namespace Ndrant\Lodashphp;
+
+final class Lodashphp
+{
+    function sayName($name)
+    {
+        return 'Halo... ' . $name . ', Welcome to new Library';
+    }
+
+    public function sumBy()
+    {
+
+    }
+}
