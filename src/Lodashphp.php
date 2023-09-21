f5bfe2adc43e299f37d0ed05a9d56767835db02e
indra heryanto <indrasids74@gmail.com>
2023-09-21 10:02:45 +0800

    first commit

--- src/Lodashphp.php
+++ src/Lodashphp.php
@@ -7,4 +7,31 @@ final class Lodashphp
     {
         return 'Halo... ' . $name . ', Welcome to new Library';
     }
+
+    function baseSum($array, $iteratee)
+    {
+        $result = null;
+        $length = count($array);
+
+        for ($index = 0; $index < $length; $index++) {
+            $current = $iteratee($array[$index]);
+            if ($current !== null) {
+                $result = $result === null ? $current : ($result + $current);
+            }
+        }
+
+        return $result;
+    }
+
+    function sumBy($array, $iteratee)
+    {
+        if ($array && count($array) > 0) {
+            $result = baseSum($array, function ($item) use ($iteratee) {
+                return $iteratee($item);
+            });
+            return $result;
+        } else {
+            return 0;
+        }
+    }
 }
---
+++ src/sumBy.php
@@ -0,0 +1,28 @@
+<?php
+
+function baseSum($array, $iteratee)
+{
+    $result = null;
+    $length = count($array);
+
+    for ($index = 0; $index < $length; $index++) {
+        $current = $iteratee($array[$index]);
+        if ($current !== null) {
+            $result = $result === null ? $current : ($result + $current);
+        }
+    }
+
+    return $result;
+}
+
+function sumBy($array, $iteratee)
+{
+    if ($array && count($array) > 0) {
+        $result = baseSum($array, function ($item) use ($iteratee) {
+            return $iteratee($item);
+        });
+        return $result;
+    } else {
+        return 0;
+    }
+}
